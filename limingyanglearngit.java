43b247815eaed9c4532678606cf206d298cd157b
AYoungA <38395877+AYoungA@users.noreply.github.com>
2018-11-16 16:39:27 +0800

    update this file
    添加一个方法

--- limingyanglearngit.java
+++ limingyanglearngit.java
@@ -1,11 +1,15 @@
 public class LMY{
-
+	
 	public static void main(String[] args) {
 		LMY l = new LMY();
 		l.method1();
+		l.method2();
 	}
 
 	public void method1(){
 		System.out.println("limingyang learning git");
+	}
+	public void method2(){
+		System.out.println("limingyang edit this file");
 	}
 }
