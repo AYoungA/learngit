a0b0a89b4bacebcb344575352d1cf5003628ec64
AYoungA <1302358940@qq.com>
2018-11-16 15:38:28 +0800

    create a file

---
+++ limingyanglearngit.java
@@ -0,0 +1,11 @@
+public class LMY{
+
+	public static void main(String[] args) {
+		LMY l = new LMY();
+		l.method1();
+	}
+
+	public void method1(){
+		System.out.println("limingyang learning git");
+	}
+}
